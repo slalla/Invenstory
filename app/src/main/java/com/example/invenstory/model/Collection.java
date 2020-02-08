5f523170aa0b39774de43c0520c1634071587f6d
Sean L <43451031+slalla@users.noreply.github.com>
2020-02-08 18:05:01 -0400

    This commit adds several variables into the Item class to make its structure more similar to the current database structure for items.
    In addition the Collection class was created to match the structure of the Collections stored in the database.
    The Collection and Item classes were documented as well.

--- app/src/main/java/com/example/invenstory/model/Collection.java
+++ app/src/main/java/com/example/invenstory/model/Collection.java
@@ -1,4 +1,31 @@
 package com.example.invenstory.model;
 
+/**
+ * The following class an Collection that may be stored in the database
+ */
 public class Collection {
+
+    /**
+     * The name of the collection. EG Video Games
+     */
+    private String name;
+
+    /**
+     * The id of the collection
+     */
+    private int id;
+
+    /**
+     * Creates a new Collection item
+     * @param name The name of the collection
+     * @param id The collection's id
+     */
+    public Collection(String name, int id){
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() { return name;}
+
+    public int getId() { return id;}
 }
--- app/src/main/java/com/example/invenstory/model/Item.java
+++ app/src/main/java/com/example/invenstory/model/Item.java
@@ -2,32 +2,80 @@ package com.example.invenstory.model;
 import java.util.Date;
 import java.util.ArrayList;
 
+/**
+ * The following class represents an object item that may be stored in the database
+ */
 public class Item {
 
+    /**
+     * The name of the item. Eg Vans Oldskool
+     */
     private String name;
+
+    /**
+     * The item's physical condition.
+     */
+    private String condition;
+
+    /**
+     * The cost of the item.
+     */
+    private String price;
+
+    /**
+     * The item's current location. Eg Bedroom
+     */
+    private String location;
+
+
+    /**
+     * The collection that the item is placed in
+     * This could potentially be implemented using a map to minimize the size of an Item
+     */
     private String groupName;
+
+    /**
+     *
+     */
     private Date initDate;
+
+    /**
+     * The item's additional attributes that are stored.
+     * May need an additional array list to keep hold of the attribute's name
+     */
     private ArrayList<String> attributes;
 
     /**
      * Creates Item with no extra attributes.
      * @param name Name of item. Ex: Vans Oldskool
      * @param groupName Name of group that item is in. Ex: Footwear
+     * @param condition The condition of the item Ex: Mint condition
+     * @param price The purchase price of the item
+     * @param location The current location of the item
      */
-    public Item(String name, String groupName) {
+    public Item(String name, String groupName, String condition, String price, String location) {
         this.name = name;
         this.groupName = groupName;
+        this.condition = condition;
+        this.price = price;
+        this.location = location;
         this.initDate = new Date();
     }
 
     /**
      * Creates Item with extra attributes.
+     * @param name Name of item. Ex: Vans Oldskool
+     * @param groupName Name of group that item is in. Ex: Footwear
+     * @param condition The condition of the item Ex: Mint condition
+     * @param price The purchase price of the item
+     * @param location The current location of the item
      * @param att List of extra attributes of the item.
      */
-    public Item(String name, String groupName, ArrayList<String> att) {
-        this(name, groupName);
+    public Item(String name, String groupName, String condition, String price, String location, ArrayList<String> att) {
+        this(name, groupName, condition, price, location);
         this.attributes = att;
     }
+
 
     public String getName() {
         return this.name;
@@ -36,6 +84,10 @@ public class Item {
     public String getGroupName() {
         return this.groupName;
     }
+
+    public String getCondition() { return  this.condition;}
+
+    public String getLocation() { return this.location;}
 
     public Date getDate() {
         return this.initDate;
