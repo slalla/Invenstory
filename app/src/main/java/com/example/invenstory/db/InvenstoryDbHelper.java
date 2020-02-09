b75b5ed8f0cb4a3ec9a3d262bbf3fe07800c8a7e
RustyTaint666 <konnercurtis11@gmail.com>
2020-02-08 20:10:00 -0400

    Added AttributesContract, DbHelper WIP

--- app/src/main/java/com/example/invenstory/db/InvenstoryDbHelper.java
+++ app/src/main/java/com/example/invenstory/db/InvenstoryDbHelper.java
@@ -1,10 +1,13 @@
 package com.example.invenstory.db;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
+
+import com.example.invenstory.model.Item;
 
 public class InvenstoryDbHelper extends SQLiteOpenHelper {
 
@@ -15,26 +18,34 @@ public class InvenstoryDbHelper extends SQLiteOpenHelper {
 
     public static final String SQL_CREATE_COLLECTION =
             "CREATE TABLE " + CollectionContract.TABLE_NAME + " (" +
-                CollectionContract.TABLE_ID + " INTEGER PRIMARY KEY," +
-                CollectionContract.COLUMN_NAME + " TEXT," +
-                CollectionContract.COLUMN_ITEM + " INTEGER," +
-                "FOREIGN KEY(" + CollectionContract.COLUMN_ITEM + ") " +
-                "REFERENCES " + ItemContract.TABLE_NAME + "(" + ItemContract.TABLE_ID + ")" +
-            ");"
+                    CollectionContract.TABLE_ID + " INTEGER PRIMARY KEY," +
+                    CollectionContract.COLUMN_NAME + " TEXT" +
+                    ");"
             ;
 
     public static final String SQL_CREATE_ITEM =
             "CREATE TABLE " + ItemContract.TABLE_NAME + " (" +
-                ItemContract.TABLE_ID + " INTEGER PRIMARY KEY," +
-                ItemContract.COLUMN_NAME + " TEXT," +
-                ItemContract.COLUMN_CONDITION + "TEXT," +
-                ItemContract.COLUMN_PRICE + " REAL," +
-                ItemContract.COLUMN_LOCATION + " TEXT," +
-                ItemContract.COLUMN_PHOTO + " BLOB," +
-                ItemContract.COLUMN_COLLECTION + " INTEGER," +
-                "FOREIGN KEY(" + ItemContract.COLUMN_COLLECTION + ") " +
-                "REFERENCES " + CollectionContract.TABLE_NAME + "(" + CollectionContract.TABLE_ID + ")" +
-            ");"
+                    ItemContract.TABLE_ID + " INTEGER PRIMARY KEY," +
+                    ItemContract.COLUMN_NAME + " TEXT," +
+                    ItemContract.COLUMN_CONDITION + "TEXT," +
+                    ItemContract.COLUMN_PRICE + " REAL," +
+                    ItemContract.COLUMN_LOCATION + " TEXT," +
+                    ItemContract.COLUMN_PHOTO + " BLOB," +
+                    ItemContract.COLUMN_COLLECTION + " INTEGER NOT NULL," +
+                    "FOREIGN KEY(" + ItemContract.COLUMN_COLLECTION + ") " +
+                    "REFERENCES " + CollectionContract.TABLE_NAME + "(" + CollectionContract.TABLE_ID + ")" +
+                    ");"
+            ;
+
+    public static final String SQL_CREATE_ATTRIBUTES =
+            "CREATE TABLE " + AttributesContract.TABLE_NAME + " (" +
+                    AttributesContract.TABLE_ID + " INTEGER PRIMARY KEY," +
+                    AttributesContract.COLUMN_NAME + " TEXT," +
+                    AttributesContract.COLUMN_VALUE + " TEXT," +
+                    AttributesContract.COLUMN_ITEM + " INTEGER NOT NULL," +
+                    "FOREIGN KEY(" + AttributesContract.COLUMN_ITEM + ") " +
+                    "REFERENCES " + ItemContract.TABLE_NAME + "(" + ItemContract.TABLE_ID + ")" +
+                    ");"
             ;
 
     public static final String SQL_DELETE_ENTRIES = "";
@@ -48,6 +59,8 @@ public class InvenstoryDbHelper extends SQLiteOpenHelper {
         try {
             db.execSQL(SQL_CREATE_COLLECTION);
             db.execSQL(SQL_CREATE_ITEM);
+            db.execSQL(SQL_CREATE_ATTRIBUTES);
+            Log.d(TAG, "Database successfully created!");
         } catch (SQLException e) {
             Log.i(TAG, "Error creating sql tables.");
             e.printStackTrace();
@@ -63,5 +76,21 @@ public class InvenstoryDbHelper extends SQLiteOpenHelper {
             e.printStackTrace();
         }
         onCreate(db);
+    }
+
+    public long addItem(Item item) {
+        SQLiteDatabase db = getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        // values.put(ItemContract.TABLE_ID, item.getId());
+        values.put(ItemContract.COLUMN_NAME, item.getName());
+        //  values.put(ItemContract.COLUMN_CONDITION, item.getCondition());
+        // values.put(ItemContract.COLUMN_PRICE, item.getValue());
+        // values.put(ItemContract.COLUMN_LOCATION, item.getLocation());
+        //TODO: Add image column
+        values.put(ItemContract.COLUMN_COLLECTION, item.getGroupName());
+
+        // returns the id of the newly create row or -1 if there was an error
+        return db.insert(ItemContract.TABLE_NAME, null, values);
     }
 }
