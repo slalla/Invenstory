5db71d735d411be69a72d7437702e3ce52cdff9c
RustyTaint666 <konnercurtis11@gmail.com>
2020-02-09 00:05:52 -0400

    Removed unused imports in MainActivity

--- app/src/main/java/com/example/invenstory/MainActivity.java
+++ app/src/main/java/com/example/invenstory/MainActivity.java
@@ -2,16 +2,7 @@ package com.example.invenstory;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.util.TimeUtils;
-
-import com.example.invenstory.db.InvenstoryDbHelper;
-import com.example.invenstory.model.Collection;
-import com.example.invenstory.model.Item;
-
-import java.util.ArrayList;
-import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
