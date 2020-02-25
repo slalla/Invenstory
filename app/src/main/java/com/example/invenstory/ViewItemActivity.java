281387c1bc2ffcb957716ef60dbb668fea1ef029
Sean L <43451031+slalla@users.noreply.github.com>
2020-02-25 01:17:03 -0400

    Updated the ViewItemActivity so that it correctly diaplayed the correct value for the test input. Added a TODO in this class as well.

--- app/src/main/java/com/example/invenstory/ViewItemActivity.java
+++ app/src/main/java/com/example/invenstory/ViewItemActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class ViewItemActivity extends AppCompatActivity {
@@ -21,8 +22,26 @@ public class ViewItemActivity extends AppCompatActivity {
         String status = "LOST";
         String purchase_info = "January 23rd 2020";
         String tag = "#Watch #Seiko";
+        String image;
+        int imageId = R.mipmap.ic_watch;
 
+        TextView nameText = findViewById(R.id.textName);
+        TextView statusText = findViewById(R.id.textStatus);
+        TextView collectionText = findViewById(R.id.textCollection);
+        TextView purchasePriceText = findViewById(R.id.textPurchasePrice);
+        TextView purchaseDateText = findViewById(R.id.textPurchaseDate);
+        TextView tagsText = findViewById(R.id.textTags);
 
+        ImageView imageImageView = findViewById(R.id.imageItemPicture);
+
+        nameText.setText(nameText.getText()+" " + name);
+        statusText.setText(statusText.getText() + " " + status);
+        collectionText.setText(collectionText.getText()+" " + collectionName);
+        purchasePriceText.setText("Purchase Price: "+ price);
+        purchaseDateText.setText("Purchase Date: " + purchase_info);
+        tagsText.setText(tagsText.getText() + " " + tag);
+
+        imageImageView.setImageResource(imageId);
 
 
     }
