7b80c01152540ecb174210842311e8670cdce005
Sean L <43451031+slalla@users.noreply.github.com>
2020-04-03 18:44:35 -0300

    Added in a carouselView that will be used for testing along with dependencies in build.gradle.

--- app/src/main/java/com/example/invenstory/ui/send/SendFragment.java
+++ app/src/main/java/com/example/invenstory/ui/send/SendFragment.java
@@ -1,9 +1,12 @@
 package com.example.invenstory.ui.send;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -14,11 +17,33 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.invenstory.Home;
 import com.example.invenstory.R;
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
+
+import java.io.File;
 
 public class SendFragment extends Fragment {
 
     private SendViewModel sendViewModel;
 
+    /**
+     * This is the carouselView that is in the fragment
+     * https://github.com/sayyam/carouselview documentation found here
+     */
+    private CarouselView carouselView;
+
+    /**
+     * This is a sample set of images from Sean's local device. This will not work
+     * Unless the same files exist on a users device. To find file paths on your device,
+     * uncomment out selectImages on the NewItemFragment and click the button
+     * Filepaths will be printed in a log message.
+     */
+    private String[] sampleImages={"/storage/emulated/0/WhatsApp/Media/WhatsApp Images/IMG-20200127-WA0000.jpg",
+            "/storage/emulated/0/WhatsApp/Media/WhatsApp Images/IMG-20200130-WA0000.jpg",
+            "/storage/emulated/0/WhatsApp/Media/WhatsApp Images/IMG-20200128-WA0002.jpeg",
+            "/storage/emulated/0/WhatsApp/Media/WhatsApp Images/IMG-20200126-WA0000.jpg",
+            "/storage/emulated/0/WhatsApp/Media/WhatsApp Images/IMG-20200129-WA0000.jpg",
+            "/storage/emulated/0/WhatsApp/Media/WhatsApp Images/IMG-20200128-WA0000.jpeg"};
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         Home.setFabOff();
@@ -28,7 +53,26 @@ public class SendFragment extends Fragment {
                 ViewModelProviders.of(this).get(SendViewModel.class);
         View root = inflater.inflate(R.layout.fragment_send, container, false);
         final TextView textView = root.findViewById(R.id.text_send);
-        sendViewModel.getText().observe(this, new Observer<String>() {
+
+        carouselView = root.findViewById(R.id.carouselView);
+
+        //Sets the images in the view
+        ImageListener imageListener = new ImageListener() {
+            @Override
+            public void setImageForPosition(int position, ImageView imageView) {
+                File imgFile = new  File(sampleImages[position]);
+
+                if(imgFile.exists()){
+                    Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+                    imageView.setImageBitmap(myBitmap);
+                }
+            }
+        };
+        //Sets the number of pages on the carousel view.
+        carouselView.setPageCount(sampleImages.length);
+        carouselView.setImageListener(imageListener);
+
+        sendViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
